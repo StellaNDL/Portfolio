e01674e549aaea8a99039eb92dabd7b45c5b1ec9
StellaNDL <b.sndlakuse@gmail.com>
2025-10-29 12:01:53 +0200

    Update Contact.jsx

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -6,19 +6,19 @@ function Contact() {
         <h1 className="sectionTitle">Contact</h1>
         <form action="" method="post">
             <div className="formGroup">
-                <label htmlFor="name">
+                <label htmlFor="name" hidden>
                      Name
                 </label>
                 <input type="text" name="name" id="name" placeholder="Name" required/>
             </div>
              <div className="formGroup">
-                <label htmlFor="email">
+                <label htmlFor="email" hidden>
                      Email
                 </label>
                 <input type="email" name="email" id="email" placeholder="Email" required/>
             </div>
              <div className="formGroup">
-                <label htmlFor="message">
+                <label htmlFor="message" hidden>
                      Message
                 </label>
                 <textarea name="message" id="message" placeholder="Message" required>
